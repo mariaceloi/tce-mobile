750ffcd145ac37d5850a657075aa15b1cce51361
joao <joaovitornepomuceno.dev@gmail.com>
2025-02-03 10:47:36 -0400

    refactor

--- app/index.tsx
+++ app/index.tsx
@@ -5,15 +5,20 @@ import ImagemDestaque from "../components/imagemDestaqueNoticia";
 import CardNoticia from "../components/cardsNoticia";
 import BoxUltimasNoticias from "../components/ultimasNoticias";
 import SVGLogoTce from "../assets/SVGLogoTce.svg";
+import SVGCabecalho from "../assets/SVGCabecalho.svg";
 
 export default function TelaNoticiasInicial() {
     return (
         <SafeAreaView style={styles.container}>
-
+            <View style={styles.cabecalho}>
+                <SVGCabecalho width={800} height={50} />
+            </View>
             <ScrollView>
+
                 <View style={styles.logo}>
                     <SVGLogoTce width={110} height={75} />
                 </View>
+
                 <ImagemDestaque />
                 <BoxUltimasNoticias />
                 <CardNoticia />
@@ -31,5 +36,11 @@ const styles = StyleSheet.create({
     logo: {
         justifyContent: 'center',
         alignItems: 'center'
+    },
+    cabecalho: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 50,
+        width: '100%',
     }
 });
