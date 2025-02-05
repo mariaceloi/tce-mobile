858315e464a469239cb7f32a1631c4d52eeab4c8
ThiagoCarassini <prothiagocarassini@gmail.com>
2025-02-05 07:20:48 -0400

    feat: :bug: resolvendo bug

--- app/index.tsx
+++ app/index.tsx
@@ -1,28 +1,30 @@
-import { View, Text, StyleSheet, ScrollView, } from "react-native";
-import LogoTce from "../components/LogoTce";
-import { SafeAreaView } from "react-native-safe-area-context";
-import ImagemDestaque from "../components/imagemDestaqueNoticia";
-import CardNoticia from "../components/cardsNoticia";
-import BoxUltimasNoticias from "../components/ultimasNoticias";
+// // import { View, Text, StyleSheet, ScrollView, } from "react-native";
+// // import LogoTce from "../components/LogoTce";
+// // import { SafeAreaView } from "react-native-safe-area-context";
+// // import ImagemDestaque from "../components/imagemDestaqueNoticia";
+// // import CardNoticia from "../components/cardsNoticia";
+// // import BoxUltimasNoticias from "../components/ultimasNoticias";
 
-export default function TelaNoticiasInicial() {
-    return (
-        <SafeAreaView style={styles.container}>
+// // export default function TelaNoticiasInicial() {
+// //     return (
+// //         <SafeAreaView style={styles.container}>
  
-            <ScrollView>
-                <LogoTce />
-                <ImagemDestaque />
-                <BoxUltimasNoticias />
-                <CardNoticia />
-            </ScrollView>
-        </SafeAreaView>
-    );
-}
+// //             <ScrollView>
+// //                 <LogoTce />
+// //                 <ImagemDestaque />
+// //                 <BoxUltimasNoticias />
+// //                 <CardNoticia />
+// //             </ScrollView>
+// //         </SafeAreaView>
+// //     );
+// }
 
-const styles = StyleSheet.create({
-    container: {
-        backgroundColor: '#F5F5F5',
-        flex: 1,
-        paddingHorizontal: 0,
-    },
-});
+// const styles = StyleSheet.create({
+//     container: {
+//         backgroundColor: '#F5F5F5',
+//         flex: 1,
+//         paddingHorizontal: 0,
+//     },
+// });
+
+//oooooooooooooooooooooooooooooooooooooooooooooooo
